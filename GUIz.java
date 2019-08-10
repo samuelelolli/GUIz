a4c239e716ec9aa9ee0c5fe0946dd6572ce26eac
Samuele Lolli <samuelelollipvt@gmail.com>
2019-08-10 16:07:14 +0200

    Update GUIz.java

--- GUIz.java
+++ GUIz.java
@@ -5,20 +5,10 @@
  */
 package guiz;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javax.swing.JApplet;
-import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import guiz.Home;
+//import guiz.Home;
 /**
  *
  * @author notebook
